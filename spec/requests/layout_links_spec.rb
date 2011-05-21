6c62bc3b4f08fefa4618768203779cee3cdbd554
dheeraj <dheeraj@futurenow.biz>
2011-05-21 20:35:39 +0530

    Finished layout and route

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -19,4 +19,8 @@ describe "LayoutLinks" do
 	get '/help'
 	response.should have_selector('title', :content => "Help")
   end
+  it "should have signup page at '/signup'" do
+	get '/signup'
+	response.should have_selector('title', :content => "Sign up")
+  end
 end
